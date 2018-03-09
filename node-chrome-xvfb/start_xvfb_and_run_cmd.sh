68afe0ce0a53f9282d9d48ff52e198c9ea104fff
Marek Trunkat <marek@trunkat.eu>
2018-03-09 11:11:21 +0100

    Using env vars in xvfb init file

--- node-chrome-xvfb/start_xvfb_and_run_cmd.sh
+++ node-chrome-xvfb/start_xvfb_and_run_cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting xvfb frame buffer"
-Xvfb :99 -ac -screen 0 1280x720x16 -nolisten tcp &
+Xvfb $DISPLAY -ac -screen 0 $XVFB_WHD -nolisten tcp &
 
 # Execute CMD (original CMD of this Dockerfile gets overriden in Actor build)
 echo "Executing Docker CMD"
