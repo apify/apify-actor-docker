035568f2501ddb9c2aaae172d324ce1a2e359de7
Jan Curn <jan.curn@gmail.com>
2018-10-19 19:15:08 +0200

    Added clarification for users that don't use main.js

--- node-chrome/main.js
+++ node-chrome/main.js
@@ -1,6 +1,14 @@
 // This file will be replaced by the content of the Act2.sourceCode field,
 // we keep this one here just for testing and clarification.
 
+console.log(
+`If you're seeing this, it means the actor started the default "main.js" file instead
+of your own source code file. You have two options how to fix this:
+1) Rename your source code file to "main.js"
+2) Define custom "package.json" and/or "Dockerfile" that will start your own source code file
+
+For more information, see https://www.apify.com/docs/actor#custom-dockerfile
+`);
 console.log('Testing node-chrome image...');
 
 const Apify = require('apify');
--- node-puppeteer/main.js
+++ node-puppeteer/main.js
@@ -1,4 +1,13 @@
 // This file will be replaced by the content of the Act2.sourceCode field,
 // we keep this one here just for testing and clarification.
 
+console.log(
+`If you're seeing this, it means the actor started the default "main.js" file instead
+of your own source code file. You have two options how to fix this:
+1) Rename your source code file to "main.js"
+2) Define custom "package.json" and/or "Dockerfile" that will start your own source code file
+
+For more information, see https://www.apify.com/docs/actor#custom-dockerfile
+`);
+
 console.log("Hello world from base Docker container for Apify Actor (with Puppeteer)");
