1d84b6852118ec7c50c031cf52ebc075b3a61237
Jan Curn <jan.curn@gmail.com>
2018-11-29 10:24:50 +0100

    Upgraded ChromeDriver to v2.44, added more tests to default main.js files

--- node-chrome/main.js
+++ node-chrome/main.js
@@ -2,28 +2,53 @@
 // we keep this one here just for testing and clarification.
 
 console.log(
-`If you're seeing this, it means the actor started the default "main.js" file instead
+`If you're seeing this text, it means the actor started the default "main.js" file instead
 of your own source code file. You have two options how to fix this:
 1) Rename your source code file to "main.js"
-2) Define custom "package.json" and/or "Dockerfile" that will start your own source code file
+2) Define custom "package.json" and/or "Dockerfile" that will run your code your way
 
 For more information, see https://www.apify.com/docs/actor#custom-dockerfile
 `);
-console.log('Testing node-chrome image...');
+console.log('Testing Docker image...');
 
 const Apify = require('apify');
 
 Apify.main(async () => {
-    const browser = await Apify.launchPuppeteer({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('http://example.com');
-    const pageTitle = await page.title();
-
-    if (pageTitle !== 'Example Domain') {
-        throw new Error(`Puppeteer test failed - returned title "${pageTitle}"" !== "Example Domain"`);
+    // First, try to open Chromium to see all dependencies are correctly installed
+    console.log('Testing Puppeteer with Chromium');
+    const browser1 = await Apify.launchPuppeteer({ headless: true });
+    const page1 = await browser1.newPage();
+    await page1.goto('http://www.example.com');
+    const pageTitle1 = await page1.title();
+    if (pageTitle1 !== 'Example Domain') {
+        throw new Error(`Puppeteer+Chromium test failed - returned title "${pageTitle1}"" !== "Example Domain"`);
     }
+    await browser1.close();
 
-    // Test "ps" command is available, sometimes it was missing in official Node builds
+    // Second, try to use full Chrome
+    console.log('Testing Puppeteer with full Chrome');
+    const browser2 = await Apify.launchPuppeteer({ headless: true, useChrome: true });
+    const page2 = await browser1.newPage();
+    await page2.goto('http://www.example.com');
+    const pageTitle2 = await page2.title();
+    if (pageTitle2 !== 'Example Domain') {
+        throw new Error(`Puppeteer+Chrome test failed - returned title "${pageTitle2}"" !== "Example Domain"`);
+    }
+    await browser2.close();
+
+    // Third, try to use WebDriver with headless Chrome
+    console.log('Testing WebDriver with full Chrome');
+    const webDriver = await Apify.launchWebDriver();
+    await webDriver.get('http://www.example.com');
+    const pageTitle3 = await webDriver.executeScript(() => {
+        return document.title;
+    });
+    if (pageTitle3 !== 'Example Domain') {
+        throw new Error(`Webdriver+Chrome test failed - returned title "${pageTitle2}"" !== "Example Domain"`);
+    }
+    await webDriver.close();
+
+    // Test that "ps" command is available, sometimes it was missing in official Node builds
     await Apify.getMemoryInfo();
 
     console.log('... test PASSED');
