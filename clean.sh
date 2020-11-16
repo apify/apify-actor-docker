e5e70cf7f2a3a02f012044d288ec695dd01ee652
Ondra Urban <ondra@apify.com>
2020-11-17 00:57:36 +0100

    Attempt to resolve npm install issues by disabling cache

--- clean.sh
+++
@@ -1,9 +0,0 @@
-
-
-echo "Removing old Docker images"
-
-docker rmi \
-  apify/actor-nodejs-basic:beta \
-  apify/actor-nodejs-basic:latest \
-  apify/actor-nodejs-chrome:latest \
-  apify/actor-nodejs-chrome:latest
--- publish_all_beta_as_latest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# fail on any error
-set -ex
-
-./publish_beta_as_latest.sh node-basic
-./publish_beta_as_latest.sh node-chrome
-./publish_beta_as_latest.sh node-chrome-xvfb
