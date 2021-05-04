1b40159b1ed4b5c43a6aae91f24892bd4ff946a8
Misha <mihails@apify.com>
2021-05-04 12:00:18 +0200

    Update old docs links (#48)

--- node-playwright/main.js
+++ node-playwright/main.js
@@ -7,26 +7,26 @@ of your own source code file. You have two options how to fix this:
 1) Rename your source code file to "main.js"
 2) Define custom "package.json" and/or "Dockerfile" that will run your code your way
 
-For more information, see https://www.apify.com/docs/actor#custom-dockerfile
+For more information, see https://docs.apify.com/actors/development/source-code#custom-dockerfile
 `);
 console.log('Testing Docker image...');
 
 const Apify = require('apify');
 const playwright = require("playwright")
-const {testChrome, testPageLoading} = require('./chrome_test');
+const { testChrome, testPageLoading } = require('./chrome_test');
 
 
 
 Apify.main(async () => {
     const browsers = ["webkit", "firefox", "chromium"]
-    const promisesHeadless = browsers.map( async browserName=>{
-        const browser = await Apify.launchPlaywright({launcher: playwright[browserName]})
+    const promisesHeadless = browsers.map(async browserName => {
+        const browser = await Apify.launchPlaywright({ launcher: playwright[browserName] })
         return testPageLoading(browser)
 
     })
 
-    const promisesHeadful = browsers.map( async browserName => {
-        const browser = await Apify.launchPlaywright({ launcher: playwright[browserName], launchOptions: {headless: false} })
+    const promisesHeadful = browsers.map(async browserName => {
+        const browser = await Apify.launchPlaywright({ launcher: playwright[browserName], launchOptions: { headless: false } })
         return testPageLoading(browser)
 
     })
@@ -39,7 +39,7 @@ Apify.main(async () => {
 
     // Try to use full Chrome with XVFB
     await testChrome({ headless: false })
-    
+
     // Test that "ps" command is available, sometimes it was missing in official Node builds
     await Apify.getMemoryInfo();
 
