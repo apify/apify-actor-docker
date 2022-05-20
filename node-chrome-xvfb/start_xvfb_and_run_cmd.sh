6b2d1cd6eff1649cfd5f0facef84aefe3525e073
Ondra Urban <ondra@apify.com>
2022-05-20 14:09:29 +0200

    Delete old images

--- node-chrome-xvfb/start_xvfb_and_run_cmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting X virtual framebuffer using: Xvfb $DISPLAY -ac -screen 0 $XVFB_WHD -nolisten tcp"
-Xvfb $DISPLAY -ac -screen 0 $XVFB_WHD -nolisten tcp &
-
-# Execute CMD (original CMD of this Dockerfile gets overridden in actor build)
-echo "Executing main command"
-exec "$@"
