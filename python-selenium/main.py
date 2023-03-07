af56956fd3441f1f262c56211437d4866f0a0707
Jiří Moravčík <jiri.moravcik@gmail.com>
2023-03-07 17:50:26 +0100

    fix quotes

--- python-selenium/main.py
+++ python-selenium/main.py
@@ -9,21 +9,21 @@ if __name__ == '__main__':
 
     print('Testing Chrome...')
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("http://www.example.com")
+    driver.get('http://www.example.com')
     assert driver.title == 'Example Domain'
     driver.quit()
 
     print('Testing Firefox...')
     firefox_options = FirefoxOptions()
-    firefox_options.add_argument("--headless")
-    firefox_options.add_argument("--no-sandbox")
-    firefox_options.add_argument("--disable-dev-shm-usage")
+    firefox_options.add_argument('--headless')
+    firefox_options.add_argument('--no-sandbox')
+    firefox_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Firefox(options=firefox_options)
-    driver.get("http://www.example.com")
+    driver.get('http://www.example.com')
     assert driver.title == 'Example Domain'
     driver.quit()
 
