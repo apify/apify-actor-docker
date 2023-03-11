7d38a648e211c9be1f5c4a6050a68cc594d7e737
František Nesveda <fnesveda@users.noreply.github.com>
2023-03-11 12:12:20 +0100

    Support running Selenium in headful mode (#98)
    This makes sure that XVFB is installed and running in both the
    Playwright and Selenium Docker images.

--- python-selenium/src/main.py
+++ python-selenium/src/main.py
@@ -5,24 +5,21 @@ from selenium import webdriver
 def main():
     print('Testing Docker image...')
 
-    print('Testing Chrome...')
-    chrome_options = ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get('http://www.example.com')
-    assert driver.title == 'Example Domain'
-    driver.quit()
+    for (browser_name, driver_class, options_class) in [('Chrome', webdriver.Chrome, ChromeOptions), ('Firefox', webdriver.Firefox, FirefoxOptions)]:
+        for headless in [True, False]:
+            print(f'Testing {browser_name}, {headless=}...')
 
-    print('Testing Firefox...')
-    firefox_options = FirefoxOptions()
-    firefox_options.add_argument('--headless')
-    firefox_options.add_argument('--no-sandbox')
-    firefox_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Firefox(options=firefox_options)
-    driver.get('http://www.example.com')
-    assert driver.title == 'Example Domain'
-    driver.quit()
+            options = options_class()
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            if headless:
+                options.add_argument('--headless')
+
+            driver = driver_class(options=options)
+
+            driver.get('http://www.example.com')
+            assert driver.title == 'Example Domain'
+
+            driver.quit()
 
     print('Tests succeeded!')
