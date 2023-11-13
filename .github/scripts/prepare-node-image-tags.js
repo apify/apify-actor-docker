266eeb3e2bb54e3a4b42aeb44cff22ff6a3b3e7c
Vlad Frangu <kingdgrizzle@gmail.com>
2023-11-13 12:34:44 +0200

    feat: automate docker images for new browsers (#119)

--- .github/scripts/prepare-node-image-tags.js
+++ .github/scripts/prepare-node-image-tags.js
@@ -1,30 +1,35 @@
 module.exports = () => {
-    const { CURRENT_NODE, LATEST_NODE, RELEASE_TAG, IMAGE_NAME, FRAMEWORK_VERSION } = process.env
+    const { CURRENT_NODE, LATEST_NODE, RELEASE_TAG, IMAGE_NAME, FRAMEWORK_VERSION, IS_LATEST_BROWSER_IMAGE } = process.env
     const tags = [];
 
-    if (CURRENT_NODE === LATEST_NODE) {
+    if (CURRENT_NODE === LATEST_NODE && IS_LATEST_BROWSER_IMAGE === 'true') {
+        // apify/actor-node-x:latest
         tags.push(`${IMAGE_NAME}:${RELEASE_TAG}`);
     }
 
-    if (RELEASE_TAG === "latest") {
-        // latest version
-
+    // latest version
+    if (RELEASE_TAG === 'latest') {
         if (FRAMEWORK_VERSION) {
+            // apify/actor-node-x:20-4.2.0
             tags.push(`${IMAGE_NAME}:${CURRENT_NODE}-${FRAMEWORK_VERSION}`)
         }
 
-        tags.push(`${IMAGE_NAME}:${CURRENT_NODE}`);
-
+        // apify/actor-node-x:20
+        // we want this only when the browser image is also latest
+        if (IS_LATEST_BROWSER_IMAGE === 'true') {
+            tags.push(`${IMAGE_NAME}:${CURRENT_NODE}`);
+        }
     } else {
         // beta and other tags
         if (FRAMEWORK_VERSION) {
+            // apify/actor-node-x:20-4.2.0-beta
             tags.push(`${IMAGE_NAME}:${CURRENT_NODE}-${FRAMEWORK_VERSION}-${RELEASE_TAG}`);
         }
 
+        // apify/actor-node-x:20-beta
+        // we don't care if the browser image is latest or not, as its a beta image
         tags.push(`${IMAGE_NAME}:${CURRENT_NODE}-${RELEASE_TAG}`);
-
     }
 
-    return { allTags: tags.join(","), firstImageName: tags[0] };
-
+    return { allTags: tags.join(','), firstImageName: tags[0] };
 }
