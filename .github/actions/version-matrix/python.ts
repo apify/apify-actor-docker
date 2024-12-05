437582a4a68955570c0acf9e669ebc8b562c5b75
Vlad Frangu <me@vladfrangu.dev>
2024-12-05 14:00:46 +0200

    chore: fix syntax for setting github output

--- .github/actions/version-matrix/python.ts
+++ .github/actions/version-matrix/python.ts
@@ -1,7 +1,7 @@
-import { semver } from 'bun';
-import { fetchPackageVersions } from './src/pypy';
+import { semver } from "bun";
+import { fetchPackageVersions } from "./src/pypy";
 
-const supportedPythonVersions = ['3.9', '3.10', '3.11', '3.12', '3.13', '3.14'];
+const supportedPythonVersions = ["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"];
 
 /**
  * Certain playwright versions will not run on newer Python versions.
@@ -10,27 +10,29 @@ const supportedPythonVersions = ['3.9', '3.10', '3.11', '3.12', '3.13', '3.14'];
  * The value is the minimum Python version that is supported.
  */
 const playwrightPythonVersionConstraints = [
-	['>=1.48.0', '>=3.13.x'], //
+	[">=1.48.0", ">=3.13.x"], //
 ];
 
-const versions = await fetchPackageVersions('playwright');
+const versions = await fetchPackageVersions("playwright");
 
 const lastFiveVersions = versions.slice(-5);
 
-console.log('Last five versions', lastFiveVersions);
+console.error("Last five versions", lastFiveVersions);
 
 const matrix = {
 	include: [] as {
-		'image-name': 'python-playwright';
-		'python-version': string;
-		'playwright-version': string;
+		"image-name": "python-playwright";
+		"python-version": string;
+		"playwright-version": string;
 	}[],
 };
 
 for (const playwrightVersion of lastFiveVersions) {
-	const maybeFilter = playwrightPythonVersionConstraints.findLast(([constraint]) => {
-		return semver.satisfies(playwrightVersion, constraint);
-	});
+	const maybeFilter = playwrightPythonVersionConstraints.findLast(
+		([constraint]) => {
+			return semver.satisfies(playwrightVersion, constraint);
+		},
+	);
 
 	for (const pythonVersion of supportedPythonVersions) {
 		if (maybeFilter) {
@@ -41,11 +43,11 @@ for (const playwrightVersion of lastFiveVersions) {
 		}
 
 		matrix.include.push({
-			'image-name': 'python-playwright',
-			'python-version': pythonVersion,
-			'playwright-version': playwrightVersion,
+			"image-name": "python-playwright",
+			"python-version": pythonVersion,
+			"playwright-version": playwrightVersion,
 		});
 	}
 }
 
-console.log(JSON.stringify(matrix));
+console.log(JSON.stringify(matrix).replaceAll('"', '\\"'));
