fda1e9feac8051eb72448944163d083c98b31e11
Vlad Frangu <me@vladfrangu.dev>
2025-08-08 14:36:27 +0200

    chore: update script

--- python-playwright/warn-myuser.sh
+++ python-playwright/warn-myuser.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-WHOAMI=`whoami`
-
 IN_PYTHON_IMAGE=false
 if [ "$1" = "python" ]; then
     IN_PYTHON_IMAGE=true
@@ -12,32 +10,12 @@ if [ "$1" = "node" ]; then
     IN_NODE_IMAGE=true
 fi
 
-if [ "$WHOAMI" != "myuser" ]; then
-    cat <<EOF
-WARNING: This Docker image is currently being built under the user '$WHOAMI'.
-
-We recommend you to build the image as the user 'myuser' instead, as it has less privileges.
-
-Please update your Dockerfile to:
-- Use 'USER myuser' directive if it isn't already
-- Use 'WORKDIR /home/myuser' directive if it isn't already
-- Copy all files with 'COPY --chown=myuser:myuser' directive if it isn't already
-
-You may also need more changes in your Dockerfile, depending on the base image you are using.
-
-If this is intentional, you can ignore this warning.
-EOF
-
-    exit 0
-fi
-
-
 if [ "$IN_PYTHON_IMAGE" = true ]; then
     cat <<EOF
-This Docker image is currently being built under the user '$WHOAMI'.
+This Docker image is now being ran as the user '$WHOAMI', and with the WORKDIR set to '/home/myuser'.
 
-If you encounter issues with your package manager, you might need to do some changes in your Dockerfile.
-Please refer to the following documentation: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
+Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
 EOF
 
     exit 0
@@ -48,7 +26,7 @@ if [ "$IN_NODE_IMAGE" = true ]; then
 The base node image is now being ran as the user '$WHOAMI', and with the WORKDIR set to '/home/myuser'.
 
 If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
-Please refer to the following documentation: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
 EOF
 
     exit 0
--- scripts/warn-myuser.sh
+++ scripts/warn-myuser.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-WHOAMI=`whoami`
-
 IN_PYTHON_IMAGE=false
 if [ "$1" = "python" ]; then
     IN_PYTHON_IMAGE=true
@@ -12,32 +10,14 @@ if [ "$1" = "node" ]; then
     IN_NODE_IMAGE=true
 fi
 
-if [ "$WHOAMI" != "myuser" ]; then
-    cat <<EOF
-WARNING: This Docker image is currently being built under the user '$WHOAMI'.
-
-We recommend you to build the image as the user 'myuser' instead, as it has less privileges.
-
-Please update your Dockerfile to:
-- Use 'USER myuser' directive if it isn't already
-- Use 'WORKDIR /home/myuser' directive if it isn't already
-- Copy all files with 'COPY --chown=myuser:myuser' directive if it isn't already
-
-You may also need more changes in your Dockerfile, depending on the base image you are using.
-
-If this is intentional, you can ignore this warning.
-EOF
-
-    exit 0
-fi
-
-
 if [ "$IN_PYTHON_IMAGE" = true ]; then
     cat <<EOF
-This Docker image is currently being built under the user '$WHOAMI'.
+This Docker image is now being ran as the user 'myuser', and with the WORKDIR set to '/home/myuser'.
 
-If you encounter issues with your package manager, you might need to do some changes in your Dockerfile.
-Please refer to the following documentation: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+You might encounter issues depending on what package manager you are using, or if you need to install some dependencies as root.
+
+If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
+Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
 EOF
 
     exit 0
@@ -45,10 +25,10 @@ fi
 
 if [ "$IN_NODE_IMAGE" = true ]; then
     cat <<EOF
-The base node image is now being ran as the user '$WHOAMI', and with the WORKDIR set to '/home/myuser'.
+The base node image is now being ran as the user 'myuser', and with the WORKDIR set to '/home/myuser'.
 
 If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
-Please refer to the following documentation: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
 EOF
 
     exit 0
