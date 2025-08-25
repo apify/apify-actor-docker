81cff5f5e68fbd112086d59217f0f2fe8531e99a
Vlad Frangu <me@vladfrangu.dev>
2025-08-25 13:59:26 +0300

    chore: throw in the docs

--- python/warn-myuser.sh
+++ python/warn-myuser.sh
@@ -12,10 +12,12 @@ fi
 
 if [ "$IN_PYTHON_IMAGE" = true ]; then
     cat <<EOF
-This Docker image is now being ran as the user '$WHOAMI', and with the WORKDIR set to '/home/myuser'.
+This Docker image is now being ran as the user 'myuser', and with the WORKDIR set to '/home/myuser'.
+
+You might encounter issues depending on what package manager you are using, or if you need to install some dependencies as root.
 
 If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
-Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+Please refer to the following documentation for how to solve common issues: https://docs.apify.com/platform/actors/development/actor-definition/dockerfile#updating-older-dockerfiles
 EOF
 
     exit 0
@@ -23,10 +25,10 @@ fi
 
 if [ "$IN_NODE_IMAGE" = true ]; then
     cat <<EOF
-The base node image is now being ran as the user '$WHOAMI', and with the WORKDIR set to '/home/myuser'.
+The base node image is now being ran as the user 'myuser', and with the WORKDIR set to '/home/myuser'.
 
 If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
-Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+Please refer to the following documentation for how to solve common issues: https://docs.apify.com/platform/actors/development/actor-definition/dockerfile#updating-older-dockerfiles
 EOF
 
     exit 0
--- scripts/warn-myuser.sh
+++ scripts/warn-myuser.sh
@@ -17,7 +17,7 @@ This Docker image is now being ran as the user 'myuser', and with the WORKDIR se
 You might encounter issues depending on what package manager you are using, or if you need to install some dependencies as root.
 
 If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
-Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+Please refer to the following documentation for how to solve common issues: https://docs.apify.com/platform/actors/development/actor-definition/dockerfile#updating-older-dockerfiles
 EOF
 
     exit 0
@@ -28,7 +28,7 @@ if [ "$IN_NODE_IMAGE" = true ]; then
 The base node image is now being ran as the user 'myuser', and with the WORKDIR set to '/home/myuser'.
 
 If you encounter any issues because of this, you may need to do some changes in your Dockerfile.
-Please refer to the following documentation for how to solve common issues: https://docs.crawlee.dev/vlad-is-writing-a-url-we-need-documentation-for-thissss
+Please refer to the following documentation for how to solve common issues: https://docs.apify.com/platform/actors/development/actor-definition/dockerfile#updating-older-dockerfiles
 EOF
 
     exit 0
