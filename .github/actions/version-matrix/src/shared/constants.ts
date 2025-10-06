da77e1424f1b17b7dc944903e78f8dbcc9c1d579
Vlada Dusek <v.dusek96@gmail.com>
2025-10-06 14:02:15 +0200

    fix: Correct access to Actor configuration property (#209)

--- .github/actions/version-matrix/src/shared/constants.ts
+++ .github/actions/version-matrix/src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const supportedPythonVersions = ['3.9', '3.10', '3.11', '3.12', '3.13'];
+export const supportedPythonVersions = ['3.10', '3.11', '3.12', '3.13'];
 
 export const supportedNodeVersions = ['18', '20', '22', '24'];
 
