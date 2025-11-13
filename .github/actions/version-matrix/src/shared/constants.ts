b258db3cf48a471814a198b2064459b179c2bb56
Vlada Dusek <v.dusek96@gmail.com>
2025-11-13 14:56:48 +0100

    feat: Add support for Python 3.14

--- .github/actions/version-matrix/src/shared/constants.ts
+++ .github/actions/version-matrix/src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const supportedPythonVersions = ['3.10', '3.11', '3.12', '3.13'];
+export const supportedPythonVersions = ['3.10', '3.11', '3.12', '3.13', '3.14'];
 
 export const supportedNodeVersions = ['18', '20', '22', '24'];
 
@@ -9,7 +9,7 @@ export const emptyMatrix = JSON.stringify({ include: [] });
 /**
  * The version of Python to be considered as the "default" version for the built image tags.
  */
-export const latestPythonVersion = '3.13';
+export const latestPythonVersion = '3.14';
 
 /**
  * The version of Node to be considered as the "default" version for the built image tags.
